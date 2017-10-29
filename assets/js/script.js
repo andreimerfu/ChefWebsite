30ecb37d4a329dcf35e22b524e21cd5845084aaf
andreimerfu <andrei.merfu@my.fmi.unibuc.ro>
2017-10-29 10:42:38 +0200

    Added dropdown button and img:hover effect at recipes section

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,23 @@ $(function() {
       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
   });
+
+
+  function dropDown() {
+    document.getElementById("retete_dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("retete-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
